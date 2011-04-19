193733a09a2ee72c94c09a63aa4a5f83aeb203a4
Leon Timmermans <fawaka@gmail.com>
2011-04-19 22:02:38 +0200

    Initial implementation

--- lib/Dist/Zilla/Plugin/BuildSelf.pm
+++ lib/Dist/Zilla/Plugin/BuildSelf.pm
@@ -1,5 +1,39 @@
 package Dist::Zilla::Plugin::BuildSelf;
-use strict;
-use warnings;
 
+use Moose;
+with qw/Dist::Zilla::Role::BuildPL Dist::Zilla::Role::TextTemplate/;
+
+use Dist::Zilla::File::InMemory;
+
+has template => (
+	is  => 'ro',
+	isa => 'Str',
+	default => "use lib 'lib';\nuse {{ \$module }} {{ \$version }};\nBuild_PL(\@ARGV);\n",
+);
+
+has version => (
+	is  => 'ro',
+	isa => 'Str',
+	default => '0.001',
+);
+
+sub register_prereqs {
+	return;
+}
+
+sub setup_installer {
+	my ($self, $arg) = @_;
+
+	(my $name = $self->zilla->name) =~ s/-/::/g;
+	my $content = $self->fill_in_string($self->template, { version => $self->version, module => $name});
+	my $file = Dist::Zilla::File::InMemory->new({ name => 'Build.PL', content => $content });
+	$self->add_file($file);
+
+	return;
+}
+
+__PACKAGE__->meta->make_immutable;
+no Moose;
 1;
+
+# ABSTRACT: Build a Build.PL that uses the current module to build itself
