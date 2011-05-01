a36553083274d967b0dc77c63b840f7c5aaf25f6
Leon Timmermans <fawaka@gmail.com>
2011-05-01 15:42:26 +0300

    Added a module attribute

--- lib/Dist/Zilla/Plugin/BuildSelf.pm
+++ lib/Dist/Zilla/Plugin/BuildSelf.pm
@@ -11,10 +11,23 @@ has template => (
 	default => "use lib 'lib';\nuse {{ \$module }} {{ \$version }};\nBuild_PL(\@ARGV);\n",
 );
 
+has module => (
+	is => 'ro',
+	isa => 'Str',
+	builder => '_module_builder',
+	lazy => 1,
+);
+
+sub _module_builder {
+	my $self = shift;
+	(my $name = $self->zilla->name) =~ s/-/::/g;
+	return $name;
+}
+
 has version => (
 	is  => 'ro',
 	isa => 'Str',
-	default => '0.001',
+	default => '',
 );
 
 sub register_prereqs {
@@ -29,8 +42,7 @@ sub register_prereqs {
 sub setup_installer {
 	my ($self, $arg) = @_;
 
-	(my $name = $self->zilla->name) =~ s/-/::/g;
-	my $content = $self->fill_in_string($self->template, { version => $self->version, module => $name});
+	my $content = $self->fill_in_string($self->template, { module => $self->module, version => $self->version });
 	my $file = Dist::Zilla::File::InMemory->new({ name => 'Build.PL', content => $content });
 	$self->add_file($file);
 
@@ -46,3 +58,20 @@ no Moose;
 =head1 DESCRIPTION
 
 Unless you're writing a Build.PL compatible module builder, you should not be looking at this. The only purpose of this module is to bootstrap any such module on Dist::Zilla.
+
+=attr module
+
+The module used to build the current module. Defaults to the main module of the current distribution.
+
+=attr version
+
+The minimal version of the module, if any. Defaults to none.
+
+=attr template
+
+The template to use for the Build.PL script. This is a Text::Template string with two arguments as described above: C<$module> and C<$version>. Default is typical for the authors Build.PL ideas, YMMV.
+
+=for Pod::Coverage
+register_prereqs
+setup_installer
+=end
